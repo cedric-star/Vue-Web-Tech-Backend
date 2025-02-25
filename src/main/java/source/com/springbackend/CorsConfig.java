fd34bd1d972091a16cdddc422853d30db07fc87e
cedric-star <cdrc.wnsch@gmail.com>
2025-02-25 09:45:06 +0100

    cors gemacht

--- src/main/java/source/com/springbackend/CorsConfig.java
+++ src/main/java/source/com/springbackend/CorsConfig.java
@@ -28,7 +28,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8085")
+                        .allowedOrigins("http://localhost:8086", "http://localhost:8085")
                         .allowedMethods("GET", "POST")
                         .allowedHeaders("*")
                         .allowCredentials(true);
